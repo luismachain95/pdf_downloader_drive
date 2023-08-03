37f6b492489493b83f61e13f2c17a84fbdc7e3d1
Luis <luismachain95@hotmail.com>
2023-08-02 23:27:02 -0300

    Limpieza de codigo y Readme

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Extension de Chrome Para Descargar Pdf de solo Lectura de Drive
+
+Esta extension agrega un boton de descarga a los pdf de solo lectura que se encuentran alojados en google drive.
+
+## Uso
+
+1 - Instale la extension (Esta repo tiene el codigo de la extension).
+
+2 - Abra el pdf que desea descargar y VISUALIZE TODO EL PDF.
+
+3- En el lado izquierdo aparecera un boton que dice "Descargar Pdf" oprima y listo, empezara la descarga.
+
+## Importante
+
+1- Tiene que visualizar todo el pdf antes de descargar.
+2- El boton aparece con los enlaces que comienzan en:
+```
+    https://drive.google.com/file/*
+``` 
+
+## Autor 
+[Luis](https://github.com/luismachain95)
+[Codigo](https://github.com/luismachain95/pdf_downloader_drive)
--- scripts/content.js
+++ scripts/content.js
@@ -1,77 +1,27 @@
-/* const article = document.querySelector("article");
-
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-} */
-
 const button = document.createElement('button');
 button.textContent = "Descargar Pdf";
-button.style.position = 'absolute';
-button.style.zIndex = '100000';
-button.style.left = '10px';
-button.style.top = '100px';
+button.title = "Recuerda visualizar todo el documento antes de descargar."
+button.classList.add('btn-downloader');
 
 button.addEventListener('click', () => {
-  /* let jspdf = document.createElement("script");
-  jspdf.onload = function () {
-      let pdf = new jsPDF();
-      let elements = document.getElementsByTagName("img");
-      for (let i in elements) {
-          let img = elements[i];
-          console.log("add img ", img);
-          if (!/^blob:/.test(img.src)) {
-              console.log("invalid src");
-              continue;
-          }
-          let can = document.createElement('canvas');
-          let con = can.getContext("2d");
-          can.width = img.width;
-          can.height = img.height;
-          con.drawImage(img, 0, 0, img.width, img.height);
-          let imgData = can.toDataURL("image/jpeg", 1.0);
-          pdf.addImage(imgData, 'JPEG', 0, 0);
-          pdf.addPage();
+  let pdf = new jsPDF();
+  let elements = document.getElementsByTagName("img");
+  for (let i in elements) {
+      let img = elements[i];
+      console.log("add img ", img);
+      if (!/^blob:/.test(img.src)) {
+          console.log("invalid src");
+          continue;
       }
-      pdf.save("Exported_File.pdf");
-  };
-  jspdf.src = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js';
-  document.body.appendChild(jspdf); */
-
-    let pdf = new jsPDF();
-    let elements = document.getElementsByTagName("img");
-    for (let i in elements) {
-        let img = elements[i];
-        console.log("add img ", img);
-        if (!/^blob:/.test(img.src)) {
-            console.log("invalid src");
-            continue;
-        }
-        let can = document.createElement('canvas');
-        let con = can.getContext("2d");
-        can.width = img.width;
-        can.height = img.height;
-        con.drawImage(img, 0, 0, img.width, img.height);
-        let imgData = can.toDataURL("image/jpeg", 1.0);
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.addPage();
-    }
-    pdf.save("Exported_File.pdf");
+      let can = document.createElement('canvas');
+      let con = can.getContext("2d");
+      can.width = img.width;
+      can.height = img.height;
+      con.drawImage(img, 0, 0, img.width, img.height);
+      let imgData = can.toDataURL("image/jpeg", 1.0);
+      pdf.addImage(imgData, 'JPEG', 0, 0);
+      pdf.addPage();
+  }
+  pdf.save("Archivo_Exportado.pdf");
 })
 document.body.appendChild(button);
